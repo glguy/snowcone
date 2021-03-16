b0078eaf16652dac5f1a6b930070c6b0d5d7578c
Eric Mertens <emertens@gmail.com>
2021-03-15 21:39:45 -0700

    do the time computation only after determining its an snote

--- lua/init.lua
+++ lua/init.lua
@@ -663,14 +663,15 @@ end
 
 function irc_handlers.NOTICE(irc)
     if string.match(irc.source, '%.') and irc.tags.time then
-        local time
-        if irc.tags.time then
-            time = string.match(irc.tags.time, '^%d%d%d%d%-%d%d%-%d%dT(%d%d:%d%d:%d%d)%.%d*Z$')
-        else
-            time = os.date '%H:%M:%S'
-        end
         local note = string.match(irc[2], '^%*%*%* Notice %-%- (.*)$')
-        if time and note then
+        if note then
+            local time
+            if irc.tags.time then
+                time = string.match(irc.tags.time, '^%d%d%d%d%-%d%d%-%d%dT(%d%d:%d%d:%d%d)%.%d*Z$')
+            else
+                time = os.date '%H:%M:%S'
+            end
+
             local event = parse_snote(time, irc.source, note)
             if event then
                 local h = handlers[event.name]
