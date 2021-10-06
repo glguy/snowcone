e145623f1f3d8e819f9b78d3bf86e106e37f8fd7
Eric Mertens <emertens@galois.com>
2021-10-06 13:04:11 -0700

    fewer allocations when writing to streams

--- app/write.c
+++ app/write.c
@@ -7,26 +7,29 @@
 #include "buffer.h"
 #include "write.h"
 
+struct request {
+    uv_write_t write;
+    uv_buf_t buf;
+    char body[];
+};
+
 static void write_done(uv_write_t *write, int status);
 
 void to_write(uv_stream_t *stream, char const* msg, size_t n)
 {
-    uv_buf_t *buf = malloc(sizeof *buf);
-    assert(buf);
-    buffer_init(buf, n);
-    memcpy(buf->base, msg, n);
-
-    uv_write_t *req = malloc(sizeof *req);
+    struct request *req = malloc(sizeof (struct request) + n);
     assert(req);
 
-    *req = (uv_write_t){ .data = buf };
-    uv_write(req, stream, buf, 1, write_done);
+    req->buf.base = req->body;
+    req->buf.len = n;
+    memcpy(req->body, msg, n);
+
+    req->write = (uv_write_t){ .data = req };
+    uv_write(&req->write, stream, &req->buf, 1, write_done);
 }
 
 static void write_done(uv_write_t *write, int status)
 {
-    uv_buf_t *buf = write->data;
-    buffer_close(buf);
-    free(buf);
-    free(write);
+    struct request *req = write->data;
+    free(req);
 }
