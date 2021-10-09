00308ad705a96fa2b77eda9df8612cb1009bd258
Eric Mertens <glguy@libera.chat>
2021-10-08 20:04:05 -0700

    fixup non-byte-aligned Masktracker

--- lua/MaskTracker.lua
+++ lua/MaskTracker.lua
@@ -5,11 +5,13 @@ M._name = 'MaskTracker'
 
 function M:_init(network, prefix)
     local bytes = prefix // 8
+    self.addrlen = #network
     self.network = string.sub(network, 1, bytes)
     local bits = prefix % 8
     if bits > 0 then
-        self.tailmask = 0x100 - (0x100 >> bits)
-        self.tail = network[bytes+1]
+        local mask = 0x100 - (0x100 >> bits)
+        self.tailmask = mask
+        self.tailbyte = string.byte(network, bytes+1) & mask
         self.tailix = bytes+1
     end
     self.count = 0
@@ -18,8 +20,8 @@ end
 function M:match(address)
     return self.addrlen == #address
        and address:startswith(self.network)
-       and (not self.tail
-            or (address[self.tailix] & self.tailmask) == self.tail)
+       and (not self.tailbyte
+            or (string.byte(address,self.tailix) & self.tailmask) == self.tailbyte)
 end
 
 function M:delta(i)
