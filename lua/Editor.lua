da7a80ffc498d888ce6d15690927f27f832c890b
Eric Mertens <glguy@libera.chat>
2021-10-21 13:40:49 -0700

    use tablex more

--- lua/Editor.lua
+++ lua/Editor.lua
@@ -1,4 +1,5 @@
 local class = require 'pl.class'
+local tablex = require 'pl.tablex'
 
 local M = class()
 M._name = 'Editor'
@@ -45,15 +46,15 @@ end
 
 function M:kill_to_beg()
     if self.yanking then
-        local t = {table.unpack(self.buffer, 1, self.cursor - 1)}
+        local t = tablex.sub(self.buffer, 1, self.cursor - 1)
         table.move(self.yank, 1, #self.yank, #t + 1, t)
         self.yank = t
     else
-        self.yank = {table.unpack(self.buffer, 1, self.cursor - 1)}
+        self.yank = tablex.sub(self.buffer, 1, self.cursor - 1)
         self.yanking = true
     end
 
-    self.buffer = {table.unpack(self.buffer, self.cursor)}
+    self.buffer = tablex.sub(self.buffer, self.cursor, -1)
     self.cursor = 1
     self:render()
 end
@@ -62,11 +63,10 @@ function M:kill_to_end()
     if self.yanking then
         table.move(self.buffer, self.cursor, #self.buffer, #self.yank + 1, self.yank)
     else
-        self.yank = {table.unpack(self.buffer, self.cursor)}
+        self.yank = tablex.sub(self.buffer, self.cursor, -1)
         self.yanking = true
     end
-
-    self.buffer = {table.unpack(self.buffer, 1, self.cursor - 1)}
+    tablex.removevalues(self.buffer, self.cursor, -1)
     self:render()
 end
 
@@ -98,10 +98,7 @@ function M:right()
 end
 
 function M:paste()
-    local t = {table.unpack(self.buffer, 1, self.cursor - 1)}
-    table.move(self.yank, 1, #self.yank, #t + 1, t)
-    table.move(self.buffer, self.cursor, #self.buffer, #t + 1, t)
-    self.buffer = t
+    tablex.insertvalues(self.buffer, self.cursor, self.yank)
     self:move(self.cursor + #self.yank)
 end
 
