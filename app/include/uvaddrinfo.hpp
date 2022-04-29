f1de33287454bf5c22eb2fc5b8f4fc27e61e2727
Eric Mertens <glguy@libera.chat>
2022-04-28 20:48:48 -0700

    nicer addrinfo wrapper

--- app/include/uvaddrinfo.hpp
+++ app/include/uvaddrinfo.hpp
@@ -3,19 +3,22 @@
 #include <uv.h>
 
 #include <iterator>
+#include <memory>
 #include <ranges>
 
 class AddrInfo {
-    addrinfo* const ai;
+    struct Deleter { void operator()(addrinfo* ai) const { uv_freeaddrinfo(ai); } };
+    std::unique_ptr<addrinfo, Deleter> ai;
 
 public:
     AddrInfo() : ai() {}
     AddrInfo(addrinfo* ai) : ai(ai) {}
-    ~AddrInfo() { uv_freeaddrinfo(ai); }
 
     template <typename T>
     class iterator_impl {
-        addrinfo* ai;
+        T* ai;
+
+        inline void next() { ai = ai->ai_next; }
     
     public:
         using iterator_category = std::input_iterator_tag;
@@ -24,15 +27,15 @@ public:
         using reference = value_type&;
         using pointer = value_type*;
 
-        iterator_impl() : ai() {}
+        explicit iterator_impl() : ai() {}
         explicit iterator_impl(addrinfo* ai) : ai(ai) {}
 
         reference operator*() const { return *ai; }
         pointer operator->() const { return ai; }
         bool operator==(iterator_impl rhs) const { return ai == rhs.ai; }
         bool operator!=(iterator_impl rhs) const { return ai != rhs.ai; }
-        iterator_impl& operator++() { ai = ai->ai_next; return *this; }
-        iterator_impl operator++(int) { auto tmp = *this; ai = ai->ai_next; return tmp; }
+        iterator_impl& operator++() { next(); return *this; }
+        iterator_impl operator++(int) { auto tmp = *this; next(); return tmp; }
     };
 
     using iterator = iterator_impl<addrinfo>;
@@ -41,17 +44,16 @@ public:
     using value_type = addrinfo;
     using reference = value_type&;
     using const_reference = value_type const&;
-    using difference_type = iterator::difference_type;
     using size_type = size_t;
 
-    auto begin() { return iterator{ai}; }
+    auto begin() { return iterator{ai.get()}; }
     auto end() { return iterator{}; }
 
-    auto begin() const { return const_iterator{ai}; }
-    auto end() const { return const_iterator{}; }
-
-    auto cbegin() const { return const_iterator{ai}; }
+    auto cbegin() const { return const_iterator{ai.get()}; }
     auto cend() const { return const_iterator{}; }
+
+    auto begin() const { return const_iterator{ai.get()}; }
+    auto end() const { return const_iterator{}; }
 };
 
 static_assert(std::ranges::input_range<AddrInfo>);
