4c33901aa43387215221775dd3f91ac752079e1a
Eric Mertens <glguy@libera.chat>
2023-09-06 14:47:43 -0700

    make ban reason searchable

--- dashboard/view/bans.lua
+++ dashboard/view/bans.lua
@@ -66,6 +66,7 @@ local function show_entry(entry)
     (conn_filter == nil or conn_filter == not entry.reason) and
     (current_filter == nil or
      safematch(entry.mask, current_filter) or
+     safematch(entry.reason, current_filter) or
      match_key(entry.nicks, current_filter))
 end
 
