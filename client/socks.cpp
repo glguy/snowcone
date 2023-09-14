c0edea52534e7b67cc848b0da07888600dfdac01
Eric Mertens <glguy@libera.chat>
2023-09-13 19:29:58 -0700

    avoid allocation and copy in socks

--- client/socks.cpp
+++ client/socks.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/asio.hpp>
 
+#include <array>
 #include <iterator>
 #include <vector>
 
@@ -47,52 +48,58 @@ auto socks_connect(
         throw std::runtime_error{"hostname too big for socks"};
     }
 
-    std::vector<uint8_t> buffer;
-
     // Send the client hello
-    buffer = {version_tag, 1, AuthMethod::NoAuth};
-    co_await boost::asio::async_write(socket, boost::asio::buffer(buffer), boost::asio::use_awaitable);
+    {
+        uint8_t buffer[] {version_tag, 1, AuthMethod::NoAuth};
+        co_await boost::asio::async_write(socket, boost::asio::buffer(buffer), boost::asio::use_awaitable);
+    }
 
     // Receive the server hello
-    buffer.resize(2); // version, method
-    co_await boost::asio::async_read(socket, boost::asio::buffer(buffer), boost::asio::use_awaitable);
-    if (buffer[0] != version_tag || buffer[1] != AuthMethod::NoAuth) {
-        throw std::runtime_error {"bad socks hello"};
+    {
+        uint8_t buffer[2]; // version, method
+        co_await boost::asio::async_read(socket, boost::asio::buffer(buffer), boost::asio::use_awaitable);
+        if (buffer[0] != version_tag || buffer[1] != AuthMethod::NoAuth) {
+            throw std::runtime_error {"bad socks hello"};
+        }
     }
 
     // Build the connect request
-    buffer = {version_tag, Command::Connect, reserved, 3, uint8_t(host.size())};
-    std::copy(host.begin(), host.end(), std::back_inserter(buffer));
-    
-    // big-endian port number
-    buffer.push_back(port >> 8);
-    buffer.push_back(port);
-
-    // Send the connect request
-    co_await boost::asio::async_write(socket, boost::asio::buffer(buffer), boost::asio::use_awaitable);
+    {
+        uint8_t request[] {version_tag, Command::Connect, reserved, 3, uint8_t(host.size())};
+        uint8_t port_buffer[] {uint8_t(port>>8), uint8_t(port)};
+        std::array<boost::asio::const_buffer, 3> const buffers {
+            boost::asio::buffer(request),
+            boost::asio::buffer(host),
+            boost::asio::buffer(port_buffer),
+        };
+        co_await boost::asio::async_write(socket, buffers, boost::asio::use_awaitable);
+    }
 
     // Receive the connect reply
-    buffer.resize(5); // version, reply, reserved, address-tag, first address byte
-    co_await boost::asio::async_read(socket, boost::asio::buffer(buffer), boost::asio::use_awaitable);
-    if (buffer[0] != version_tag || buffer[1] != Reply::Succeeded)
+    uint8_t reply[5]; // version, reply, reserved, address-tag, first address byte
+    co_await boost::asio::async_read(socket, boost::asio::buffer(reply), boost::asio::use_awaitable);
+    if (reply[0] != version_tag || reply[1] != Reply::Succeeded)
     {
         throw std::runtime_error{"socks request unsuccessful"};
     }
 
     // Ignore the returned address (first byte already in buffer)
-    switch (buffer[3]) {
-        case 1:
-            buffer.resize(3+2); // ipv4 + port
+    switch (reply[3]) {
+        case 1: {
+            uint8_t buffer[5]; // ipv4 + port
             co_await boost::asio::async_read(socket, boost::asio::buffer(buffer), boost::asio::use_awaitable);
             break;
-        case 3:
-            buffer.resize(size_t(buffer[4]) + 2); // domain name + port
+        }
+        case 3: {
+            std::vector<uint8_t> buffer(size_t(reply[4]) + 2); // domain name + port
             co_await boost::asio::async_read(socket, boost::asio::buffer(buffer), boost::asio::use_awaitable);
             break;
-        case 4:
-            buffer.resize(15+2); // ipv6 + port
+        }
+        case 4: {
+            uint8_t buffer[17]; // ipv6 + port
             co_await boost::asio::async_read(socket, boost::asio::buffer(buffer), boost::asio::use_awaitable);
             break;
+        }
         default:
             throw std::runtime_error {"bad socks confirmation address type"};
     }
