8bbf403c70b02cd8cd974cbb1e7ae9adb6d5392e
Eric Mertens <glguy@libera.chat>
2023-09-19 20:04:31 -0700

    socks5 decode remote endpoint

--- client/irc/socks.cpp
+++ client/irc/socks.cpp
@@ -44,4 +44,34 @@ std::string SocksErrCategory::message(int ev) const
     }
 }
 
+namespace detail
+{
+
+auto push_host(Host const& host, std::vector<uint8_t> &buffer) -> void
+{
+    switch (host.index()) {
+        case 0: {
+            auto const& hostname = std::get<0>(host);
+            buffer.push_back(uint8_t(AddressType::DomainName));
+            buffer.push_back(hostname.size());
+            std::copy(hostname.begin(), hostname.end(), std::back_inserter(buffer));
+            break;
+        }
+        case 1: {
+            auto const& address = std::get<1>(host);
+            if (address.is_v4()) {
+                buffer.push_back(uint8_t(AddressType::IPv4));
+                auto const bytes = address.to_v4().to_bytes();
+                std::copy(bytes.begin(), bytes.end(), std::back_inserter(buffer));
+            } else {
+                buffer.push_back(uint8_t(AddressType::IPv6));
+                auto const bytes = address.to_v6().to_bytes();
+                std::copy(bytes.begin(), bytes.end(), std::back_inserter(buffer));
+            }
+        }
+    }
+}
+
+} // namespace detail
+
 } // namespace socks5
