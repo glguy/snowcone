fba1b2ec46696ff5251add26a7c197732f72470f
Eric Mertens <glguy@libera.chat>
2023-09-20 09:39:08 -0700

    socks5 endpoint offset off-by-one

--- client/irc/socks.hpp
+++ client/irc/socks.hpp
@@ -3,11 +3,12 @@
 #include <boost/asio.hpp>
 #include <boost/endian.hpp>
 
-#include <array>
 #include <cstdint>
+#include <cstring>
 #include <string>
 #include <string_view>
 #include <variant>
+#include <stdexcept>
 
 namespace socks5
 {
@@ -66,37 +67,38 @@ struct SocksImplementation
 {
     boost::asio::ip::tcp::socket& socket_;
     Host const host_;
-    uint16_t const port_;
+    boost::endian::big_uint16_t const port_;
 
     /// buffer used to back async read/write operations
     std::vector<uint8_t> buffer_;
 
-    // Intermediate states used to dispatch to different operator() implementations
+    // Representations of states in the protocol
+    struct Start{};
     struct RecvReply{};
     struct RecvHello{};
     struct SendConnect{};
     struct RecvAddress{};
     struct Finish{};
 
-    // In the case of error codes, abort the process. Otherwise proceed
-    // without the error code. This allows all the read/write error handling
-    // to be factored out into one place.
-    template <typename Self, typename... Ts>
-    void operator()(Self& self, boost::system::error_code const error, Ts&&... t)
+    // In the case of error codes, abort the process.
+    // Disregard the bytes_transferred.
+    // Dispatch to the handler function for the target state
+    template <typename Self, typename State = Start>
+    void operator()(Self& self, boost::system::error_code const error = {}, std::size_t = {}, State state = {})
     {
         if (error)
         {
-            self.complete(error, {}, {});
+            self.complete(error, {});
         }
         else
         {
-            (*this)(self, std::forward<Ts>(t)...);
+            step(self, state);
         }
     }
 
-    // START: Send hello and offer authentication methods
+    // Send hello and offer authentication methods
     template <typename Self>
-    void operator()(Self& self)
+    void step(Self& self, Start)
     {
         buffer_ = {version_tag, 1 /* number of methods */, uint8_t(AuthMethod::NoAuth)};
         boost::asio::async_write(socket_, boost::asio::buffer(buffer_),
@@ -105,11 +107,8 @@ struct SocksImplementation
 
     // Waiting for server to choose authentication method
     template <typename Self>
-    void operator()(
-        Self& self,
-        std::size_t,
-        RecvHello
-    ) {
+    void step(Self& self, RecvHello)
+    {
         buffer_.resize(2); // version, method
         boost::asio::async_read(socket_, boost::asio::buffer(buffer_),
             std::bind(std::move(self), _1, _2, SendConnect{}));
@@ -117,18 +116,15 @@ struct SocksImplementation
 
     // Send TCP connection request for the domain name and port
     template <typename Self>
-    void operator()(
-        Self& self,
-        std::size_t,
-        SendConnect
-    ) {
+    void step(Self& self, SendConnect)
+    {
         if (version_tag != buffer_[0])
         {
-            self.complete(boost::system::errc::make_error_code(boost::system::errc::not_supported), {}, {});
+            self.complete(boost::system::errc::make_error_code(boost::system::errc::not_supported), {});
             return;
         }
         if (AuthMethod::NoAuth != static_cast<AuthMethod>(buffer_[1])) {
-            self.complete(boost::system::errc::make_error_code(boost::system::errc::not_supported), {}, {});
+            self.complete(boost::system::errc::make_error_code(boost::system::errc::not_supported), {});
             return;
         }
 
@@ -137,10 +133,8 @@ struct SocksImplementation
             uint8_t(Command::Connect),
             reserved,
         };
-
         push_host(host_, buffer_);
-        buffer_.push_back(port_ >> 8);
-        buffer_.push_back(port_);
+        std::copy_n(port_.data(), 2, std::back_inserter(buffer_));
 
         boost::asio::async_write(socket_, boost::asio::buffer(buffer_),
             std::bind(std::move(self), _1, _2, RecvReply{}));
@@ -148,88 +142,79 @@ struct SocksImplementation
 
     // Wait for response to the connection request
     template <typename Self>
-    void operator()(
-        Self& self,
-        std::size_t,
-        RecvReply
-    ) {
-        buffer_.resize(5); // version, reply, reserved, address-tag, first address byte
+    void step(Self& self, RecvReply)
+    {
+        buffer_.resize(4); // version, reply, reserved, address-tag
         boost::asio::async_read(socket_, boost::asio::buffer(buffer_),
             std::bind(std::move(self), _1, _2, RecvAddress{}));
     }
 
     // Waiting on the remaining variable-sized address portion of the response
     template <typename Self>
-    void operator()(
-        Self& self,
-        std::size_t,
-        RecvAddress
-    ) {
+    void step(Self& self, RecvAddress)
+    {
         if (version_tag != buffer_[0])
         {
-            self.complete(boost::system::errc::make_error_code(boost::system::errc::not_supported), {}, {});
+            self.complete(boost::system::errc::make_error_code(boost::system::errc::not_supported), {});
             return;
         }
         auto const reply = static_cast<SocksErrc>(buffer_[1]);
         if (SocksErrc::Succeeded != reply)
         {
-            self.complete(boost::system::error_code{int(reply), theSocksErrCategory}, {}, {});
+            self.complete(boost::system::error_code{int(reply), theSocksErrCategory}, {});
             return;
         }
 
         std::size_t n;
         switch (static_cast<AddressType>(buffer_[3])) {
             case AddressType::IPv4:
-                n = 5; // ipv4 + port
+                n = 6; // ipv4 + port
                 break;
             case AddressType::IPv6:
-                n = 17; // ipv6 + port
+                n = 18; // ipv6 + port
                 break;
             default:
-                self.complete(boost::system::errc::make_error_code(boost::system::errc::not_supported), {}, {});
+                self.complete(boost::system::errc::make_error_code(boost::system::errc::not_supported), {});
                 return;
         }
 
-        buffer_.resize(5 + n);
-        boost::asio::async_read(socket_, boost::asio::buffer(&buffer_[5], n),
+        buffer_.resize(4 + n);
+        boost::asio::async_read(socket_, boost::asio::buffer(&buffer_[4], n),
             std::bind(std::move(self), _1, _2, Finish{}));
     }
 
     // Protocol complete! Return the client's remote endpoint
     template <typename Self>
-    void operator()(
-        Self& self,
-        std::size_t,
-        Finish
-    ) {
-        auto const n = buffer_.size();
-        uint16_t port = buffer_[n-2] << 8 | buffer_[n-1];
-    
+    void step(Self& self, Finish)
+    {
+        std::size_t const addr_offset = 4;
         switch (static_cast<AddressType>(buffer_[3]))
         {
             case AddressType::IPv4: {
                 boost::asio::ip::address_v4::bytes_type bytes;
-                std::copy_n(&buffer_[5], 4, bytes.begin());
-                self.complete({}, boost::asio::ip::make_address_v4(bytes), port);
+                boost::endian::big_uint16_t port;
+                std::memcpy(bytes.data(), &buffer_[addr_offset], 4);
+                std::memcpy(port.data(), &buffer_[addr_offset + 4], 2);
+                self.complete({}, {boost::asio::ip::make_address_v4(bytes), port});
                 break;
             }
             case AddressType::IPv6: {
                 boost::asio::ip::address_v6::bytes_type bytes;
-                std::copy_n(&buffer_[5], 16, bytes.begin());
-                self.complete({}, boost::asio::ip::make_address_v6(bytes), port);
+                boost::endian::big_uint16_t port;
+                std::memcpy(bytes.data(), &buffer_[addr_offset], 16);
+                std::memcpy(port.data(), &buffer_[addr_offset + 16], 2);
+                self.complete({}, {boost::asio::ip::make_address_v6(bytes), port});
                 break;
             }
             default:
-                self.complete(boost::system::errc::make_error_code(boost::system::errc::not_supported), {}, {});
-                return;
+                throw std::logic_error{"socks5 finish state with invalid address type"};
         }
-
     }
 };
 
 } // namespace detail
 
-using Signature = void(boost::system::error_code, boost::asio::ip::address, uint16_t);
+using Signature = void(boost::system::error_code, boost::asio::ip::tcp::endpoint);
 
 /// @brief Asynchronous SOCKS5 connection request
 /// @tparam CompletionToken Token accepting: error_code, address, port
