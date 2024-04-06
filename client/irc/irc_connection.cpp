ba58c506af12be2219a18e8d6413ba317521529a
Eric Mertens <glguy@libera.chat>
2024-04-06 11:24:27 -0700

    move implementation to the cpp

--- client/irc/irc_connection.cpp
+++ client/irc/irc_connection.cpp
@@ -32,3 +32,51 @@ auto irc_connection::write(std::string_view const cmd, int const ref) -> void
         write_timer.cancel_one();
     }
 }
+
+auto irc_connection::write_thread() -> void
+{
+    if (write_buffers.empty())
+    {
+        write_timer.async_wait(
+            [weak = weak_from_this()](auto)
+            {
+                if (auto const self = weak.lock())
+                {
+                    if (not self->write_buffers.empty())
+                    {
+                        self->write_thread_actual();
+                    }
+                }
+            }
+        );
+    }
+    else
+    {
+        write_thread_actual();
+    }
+}
+
+auto irc_connection::write_thread_actual() -> void
+{
+    boost::asio::async_write(
+        stream_,
+        write_buffers,
+        [weak = weak_from_this(), n = write_buffers.size()]
+        (boost::system::error_code const& error, std::size_t)
+        {
+            if (not error)
+            {
+                if (auto const self = weak.lock())
+                {
+                    for (std::size_t i = 0; i < n; i++)
+                    {
+                        luaL_unref(self->L, LUA_REGISTRYINDEX, self->write_refs.front());
+                        self->write_buffers.pop_front();
+                        self->write_refs.pop_front();
+                    }
+
+                    self->write_thread();
+                }
+            }
+        });
+}
--- client/irc/irc_connection.hpp
+++ client/irc/irc_connection.hpp
@@ -49,54 +49,10 @@ public:
 
     static std::size_t const irc_buffer_size = 131'072;
 
-    // There's data now, actually write it
-    auto write_thread_actual() -> void
-    {
-        boost::asio::async_write(
-            stream_,
-            write_buffers,
-            [weak = weak_from_this(), n = write_buffers.size()]
-            (boost::system::error_code const& error, std::size_t)
-            {
-                if (not error)
-                {
-                    if (auto const self = weak.lock())
-                    {
-                        for (std::size_t i = 0; i < n; i++)
-                        {
-                            luaL_unref(self->L, LUA_REGISTRYINDEX, self->write_refs.front());
-                            self->write_buffers.pop_front();
-                            self->write_refs.pop_front();
-                        }
-
-                        self->write_thread();
-                    }
-                }
-            });
-    }
-
     // Either write data now or wait for there to be data
-    auto write_thread() -> void
-    {
-        if (write_buffers.empty())
-        {
-            write_timer.async_wait(
-                [weak = weak_from_this()](auto)
-                {
-                    if (auto const self = weak.lock())
-                    {
-                        if (not self->write_buffers.empty())
-                        {
-                            self->write_thread_actual();
-                        }
-                    }
-                }
-            );
-        }
-        else
-        {
-            write_thread_actual();
-        }
-    }
-};
+    auto write_thread() -> void;
 
+private:
+    // There's data now, actually write it
+    auto write_thread_actual() -> void;
+};
